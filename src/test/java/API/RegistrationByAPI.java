21f111c74c98c08850b3f8a951c56a965629d257
GenSteel87 <gensteel87@gmail.com>
2023-04-09 12:26:42 +0300

    changes

--- src/test/java/API/RegistrationByAPI.java
+++ src/test/java/API/RegistrationByAPI.java
@@ -1,27 +1,20 @@
 package API;
 
-
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.mapper.ObjectMapperType;
 import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
-import org.asynchttpclient.Request;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.given;
-
 
 public class RegistrationByAPI {
     @Test
     public void testAPI(){
+
         NewUser newUser = new NewUser("eve.holt@reqres.in", "pistol");
 
         String getUrl = "https://reqres.in";
         String basePathReg = "/api/register";
         String basePathDel = "/api/users/";
-
 
         Response res = given()
                 .baseUri(getUrl)
@@ -36,11 +29,7 @@ public class RegistrationByAPI {
         Resp actualRes = res.getBody().as(Resp.class);
         Assert.assertEquals(actualRes.getId(),expectedRes.getId());
 
-//        Response req = given()
-//                .queryParam("page",2)
-//                .get(getUrl + basePath);
-
-        System.out.println("created status " + res.getStatusCode());
+        System.out.println("User creation - response status is " + res.getStatusCode());
 
         Response del = given()
                 .baseUri(getUrl)
@@ -51,7 +40,6 @@ public class RegistrationByAPI {
 
         Assert.assertEquals(del.getStatusCode(),204 );
 
-        System.out.println("delete response " + del.statusCode());
 
 
     }
